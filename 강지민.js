e0c301c586cba87378a1d85ced1dff7c6c7b2ea4
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-04-30 18:50:19 +0900

    removechild 객체에 접근할 때 삭제하고자 하는 요소의 바로 위 부모의 객체에 접근 해야함

--- 강지민.js
+++ 강지민.js
@@ -2,6 +2,10 @@ const first = document.getElementById('first');
 const second = document.getElementById('second');
 const moveButton = document.getElementById('move-button');
 const root = document.getElementById('root');
+const root1 = document.getElementById('root').children[0].children[0];
+const root2 = document.getElementById('root').children[1].children[0];
+
+
 
 // Q.1 식별자 moveButton을 누르면, first의 첫번째 자식을 second의 마지막 자식으로 이동시키는 코드를 작성해주세요.
 // Q.2 #first 의 ul 태그의 자식들이(li태그)가 모두 second로 이동했다면,
@@ -9,7 +13,13 @@ const root = document.getElementById('root');
 // Q.4 1초뒤에 "hello!" 라는 문구가 나타나게 하세요.
 
 moveButton.addEventListener('click',()=>{
+let text = root.children[0].children[0].children[0].textContent
+root1.removeChild(root.children[0].children[0].children[0])
+let li = document.createElement('li');
+li.textContent=text;
+root2.appendChild(li)
 
 })
 
 console.dir(root)
+console.dir(root2.children[0])
